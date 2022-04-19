8a225e23bfec3a2f46f9754cb683865f2943756e
bemuse01 <sequeland@gmail.com>
2022-04-19 11:19:50 +0900

    Revert "update"
    This reverts commit ceb5c5f9f0f2bee27b1ce90e3a3fdf84181c1c4e.

--- assets/js/component/ui-playlist/navCurrent.js
+++ assets/js/component/ui-playlist/navCurrent.js
@@ -44,16 +44,12 @@ export default {
 
             </div>
 
-            <div class="song-difficulty">
-                
-                <div class="difficulty-bg">
-                    <img src="./assets/src/songselect_difficulty_bg_easy.png">
-                </div>
+            <div class="nav-difficulty difficulty-bg">
+                <img src="./assets/src/songselect_difficulty_bg_easy.png">
+            </div>
 
-                <div class="difficulty">
-                    <img src="./assets/src/songselect_difficulty_easy.png">
-                </div>
-
+            <div class="nav-difficulty difficulty">
+                <img src="./assets/src/songselect_difficulty_easy.png">
             </div>
 
         </div>
