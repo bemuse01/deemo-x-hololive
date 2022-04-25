c27a30413f495b92c046c88efde0fd153a0c0940
bemuse01 <sequeland@gmail.com>
2022-04-25 21:10:33 +0900

    update

--- assets/js/component/container/canvasContainer.js
+++ assets/js/component/container/canvasContainer.js
@@ -1,7 +1,23 @@
 export default {
     template: `
-        <div id="canvas-container">
+        <div id="canvas-container" :style="style">
             <canvas id="canvas"></canvas>
         </div>
-    `
+    `,
+    setup(){
+        const {computed} = Vue
+        const {useStore} = Vuex
+
+        const store = useStore()
+        const crtKey = computed(() => store.getters['playlist/getCrtKey'])
+        const crtItem = computed(() => store.getters['playlist/getSong'](crtKey.value))
+        const style = computed(() => {
+            if(+crtItem.value.type === 2) return {filter: `drop-shadow(0 0 2px #${crtItem.value.color.toString(16)}) brightness(1.5)`}
+            else return {filter: 'none'}
+        })
+
+        return{
+            style
+        }
+    }
 }
--- assets/js/component/ui-visualizer/v2Container.js
+++ assets/js/component/ui-visualizer/v2Container.js
@@ -23,7 +23,7 @@ export default {
         })
 
         onUnmounted(() => {
-
+            visualizer.value.dispose()
         })
 
         return{
