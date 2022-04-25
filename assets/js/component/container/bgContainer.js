e2b35929857d37307a4ff57bfe00cd90274e36c7
bemuse01 <sequeland@gmail.com>
2022-04-25 19:13:02 +0900

    Update bgContainer.js

--- assets/js/component/container/bgContainer.js
+++ assets/js/component/container/bgContainer.js
@@ -2,25 +2,36 @@ export default {
     template: `
         <transition >
             <div id="bg-container">
-                <div class="bg-img"></div>
-                <div class="bg-overlay"></div>
+                <div class="bg-img" :style="imgStyle"></div>
+                <div class="bg-overlay" :style="overlayStyle"></div>
             </div>
         </transition>
     `,
     setup(){
+        const {computed} = Vue
         const {useStore} = Vuex
 
-        // const store = useStore()
-        // const playing = computed(() => store.getters['playlist/getPlaying'])
-        // const crtKey = computed(() => store.getters['playlist/getCrtKey'])
-        // const crtItem = computed(() => store.getters['playlist/getSong'](crtKey.value))
-        // const imgStyle = computed(() => ({
-        //     background: `url('${crtItem.value.bgSrc}') no-repeat center center / cover`
-        // }))
-
+        const store = useStore()
+        const crtKey = computed(() => store.getters['playlist/getCrtKey'])
+        const crtItem = computed(() => store.getters['playlist/getSong'](crtKey.value))
+        const imgStyle = computed(() => {
+            if(crtItem.value.isDefault){
+                return {background: 'none'}
+            }else{
+                return {background: `url('${crtItem.value.bgSrc}') no-repeat center center / cover`}
+            }
+        })
+        const overlayStyle = computed(() => {
+            if(crtItem.value.isDefault){
+                return {background: 'none'}
+            }else{
+                return {background: `rgba(0, 0, 0, 0.8)`}
+            }
+        })
 
         return{
-            // imgStyle
+            imgStyle,
+            overlayStyle
         }
     }
 }
