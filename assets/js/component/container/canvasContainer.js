60762bf24a82695c60b4b9510bf3e7284a9752f3
bemuse01 <sequeland@gmail.com>
2022-04-25 21:16:40 +0900

    Update canvasContainer.js

--- assets/js/component/container/canvasContainer.js
+++ assets/js/component/container/canvasContainer.js
@@ -12,7 +12,13 @@ export default {
         const crtKey = computed(() => store.getters['playlist/getCrtKey'])
         const crtItem = computed(() => store.getters['playlist/getSong'](crtKey.value))
         const style = computed(() => {
-            if(+crtItem.value.type === 2) return {filter: `drop-shadow(0 0 2px #${crtItem.value.color.toString(16)}) brightness(1.5)`}
+            if(+crtItem.value.type === 2) return {
+                filter: `
+                    drop-shadow(0 0 2px #${crtItem.value.color.toString(16)}) 
+                    drop-shadow(0 0 2px #${crtItem.value.color.toString(16)}) 
+                    brightness(1.5)
+                `
+            }
             else return {filter: 'none'}
         })
 
